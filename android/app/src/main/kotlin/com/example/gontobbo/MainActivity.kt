c2f6d4c30fb7f100cc70da2844dfcddc1a4eacaf
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2023-05-28 01:45:12 +0600

    --setup project

---
+++ android/app/src/main/kotlin/com/example/gontobbo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gontobbo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  SafeArea(child: Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: const SingleChildScrollView(
+        child: Center(
+
+        ),
+      ),
+    ));
+  }
+}
